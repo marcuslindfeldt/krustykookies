a78dda384551ddf731524061448614a6771e8763
ymerdawe <root@buf>
2013-03-21 04:04:07 +0100

    OrderMapper: save delete
    Order:
    Construct
    toArray
    toJson

--- src/Krusty/Model/Mapper/OrderMapper.php
+++ src/Krusty/Model/Mapper/OrderMapper.php
@@ -3,18 +3,44 @@
 namespace Krusty\Model\Mapper;
 
 use \Krusty\Model\Order,
-	\Krusty\Model\Mapper\AbstractMapper;
+\Krusty\Model\Mapper\AbstractMapper;
 
 class OrderMapper extends AbstractMapper
 {
-	public function save($id)
+	//sets the order with order id $id to delivered=1, does not check the input
+	public function setDelivered($id){
+		
+	}
+	//Adds a new order, does not check for foregin key constraint or check the input
+	public function save($id, $customer, $deadline)
 	{
-		# code...
+		try {
+			$db = $this->getAdapter();
+			$stmt = $db->prepare('INSERT INTO Orders VALUES(:id, :customer, :deadline, null)');
+			$stmtArray=array();
+			$stmtArray['id']=$id;
+			$stmtArray['customer']=$customer;
+			$stmtArray['deadline']=$deadline;
+			$stmt->execute($stmtArray);
+			return true;
+		} catch (Exception $e) {
+			return false;
+		}
 	}
 
+	//Deletes an order with the specific id
 	public function delete($id)
 	{
-		# code...
+		try {
+			$db = $this->getAdapter();
+			$stmt = $db->prepare('DELETE FROM Orders WHERE order_id = :id');
+			$stmtArray=array();
+			$stmtArray['id']=$id;
+			$stmt->execute($stmtArray);
+			return true;
+		} catch (Exception $e) {
+			return false;
+		}
 	}
 
 	// Should return an order object that contains a Customer object
--- src/Krusty/Model/Order.php
+++ src/Krusty/Model/Order.php
@@ -6,20 +6,28 @@ use Krusty\Model\Customer;
 
 class Order
 {
-	private $id;
+	private $order_id;
 	private $customer;
-
+	private $deadline;
+		
+	public function Order($id, $customer, $order_id){
+		$this->order_id=$order_id;
+		$this->customer=$customer;
+		$this->deadline=$deadline;
+	}
+	
 	public function toArray()
 	{
 		$arr=array();
-		$arr['id']=$id;
-		$arr['customer']=$customer;
+		$arr['order_id']=$this->id;
+		$arr['customer']=$this->customer;
+		$arr['deadline']=$this->deadline;
 		return $arr;
 	}
 
 	public function toJson()
 	{
-		# code...
+		return jason_encode($this->toArry());
 	}
 
 }
