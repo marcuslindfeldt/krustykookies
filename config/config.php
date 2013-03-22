85e3b9ea415f65a1d9c5e68d4641471d970f4db6
Marcus Lindfeldt <marcus.lindfeldt@gmail.com>
2013-03-22 16:10:46 +0100

    deleted config

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "loffasliten");
-define("DB_NAME", "krusty");
-
-define("TEMPLATE_DIR", __DIR__ . '/../templates/');
